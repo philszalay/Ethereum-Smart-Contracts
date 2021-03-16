1ccc8d2795be92e1e507881ee771e275f95a094d
philszalay <philszalay@aol.de>
2021-03-16 21:52:49 +0100

    add Owner contract, remove build folder

---
+++ contracts/Owned.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.5.16;
+
+contract Owned {
+    address public owner;
+
+    // constructor
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,7 +2,9 @@
 
 pragma solidity 0.5.16;
 
-contract ToDoList {
+import 'Owned.sol';
+
+contract ToDoList is Owned {
     uint256 public constant maxAmountOfTodosForUser = 100;
 
     // owner => array of Todos
@@ -18,11 +20,6 @@ contract ToDoList {
         address owner;
         bool isDone;
     }
-    
-    modifier onlyOwner(address _owner) {
-        require(msg.sender == _owner);
-        _;
-    }
 
     function addTodo(bytes32 _content) public {
         Todo memory newTodo = Todo(lastIds[msg.sender], _content, block.timestamp, msg.sender, false);
@@ -30,7 +27,7 @@ contract ToDoList {
     }
     
     // A modifier can be added to a function and gets executed first. It handles the access to the function.
-    function markTodoAsDone(uint256 _id) public onlyOwner(todos[msg.sender][_id].owner) {
+    function markTodoAsDone(uint256 _id) public onlyOwner() {
         require(_id <= maxAmountOfTodosForUser);
         require(!todos[msg.sender][_id].isDone);
         
